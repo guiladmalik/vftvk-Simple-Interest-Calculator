4d2f7360fe7123a17a5b492beba1ea59ea03848f
guiladmalik <102885248+guiladmalik@users.noreply.github.com>
2022-04-17 23:18:52 +0300

    the new code i made in the assignment yay!

--- script.js
+++ script.js
@@ -1,6 +1,46 @@
-function compute()
-{
-    p = document.getElementById("principal").value;
-    
+var slider = document.getElementById ("myrange");
+var output = document.getElementById ("value");
+var result= document.getElementById("result");
+var presult= document.getElementById("principal_result");
+var rresult= document.getElementById("range_result");
+var yresult= document.getElementById("years_result");
+let currentyear= new Date().getFullYear();
+
+var anountContainer = document.getElementById("amount-button");
+anountContainer.style.display = "none";
+
+output.innerHTML= slider.value;
+ 
+slider.oninput = function() {
+    output.innerHTML= this.value;  
 }
-        
+
+var principal = document.getElementById("principal");
+var range = slider.value;
+var years = document.getElementById("years");
+
+
+
+const getRange = (event)=> {
+    range = event.target.value;
+}
+
+const compute = () =>{ 
+
+    if(Number(principal.value)  < 1 ){
+        anountContainer.style.display = "block";
+        return;
+    }
+    presult.innerHTML= "If you deposit "+ principal.value;
+    rresult.innerHTML= "at an interest rate of "+range+"%";
+    let interest = principal.value * years.value * range /100;
+    result.innerHTML ="you will recieve an amount of " +interest;
+    let year = currentyear + Number(years.value);
+    yresult.innerHTML="in the year "+year+ "."; 
+
+}
+
+const amountButton = () => {
+    anountContainer.style.display = "none";
+    principal.focus();
+}
